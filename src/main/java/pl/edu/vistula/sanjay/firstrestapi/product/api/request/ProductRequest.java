da04e3530ab54359d117f99f21a66adf690eef99
IamSanjayBalakrishnan <164213098+IamSanjayBalakrishnan@users.noreply.github.com>
2026-05-25 20:55:00 +0200

    Delete src directory

--- src/main/java/pl/edu/vistula/sanjay/firstrestapi/product/api/request/ProductRequest.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.vistula.sanjay.firstrestapi.product.api.request;
-
-public class ProductRequest {
-    private String name;
-
-    public ProductRequest() {}
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-}
--- src/main/java/pl/edu/vistula/sanjay/firstrestapi/product/support/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.vistula.sanjay.firstrestapi.product.support.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(Long id) {
-        super("Could not locate product record matching tracking ID: " + id);
-    }
-}
